e043f5ec88fb57761e7aa931b54a28afd828bd0e
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-08-23 17:42:43 +0300

    commit server changes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,31 @@ import studentsRoutes from './routes/students.js'
 import adminRoutes from './routes/admin.js'
 dotenv.config()
 const app = express()
-app.use(cors())
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://helaha-website.vercel.app',
+]
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests without an Origin
+        if (!origin) {
+            return callback(null, true)
+        }
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true)
+        }
+        return callback(new Error('Not allowed by CORS'))
+    },
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
+app.options('*', cors())
 app.use(express.json())
 app.use('/api/students', studentsRoutes)
 app.use('/api/admin', adminRoutes)
 app.get('/', (req, res) => {
-    res.json({ message: 'Server is working' })
+    res.json({
+        message: 'Server is working',
+    })
 })
 export default app
