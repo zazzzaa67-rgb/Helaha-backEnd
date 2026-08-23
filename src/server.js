6c0c3c51d99f64e0522a785d57c11c1587958ebd
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-08-23 16:30:56 +0300

    commit backEnd changes

--- src/server.js
+++ src/server.js
@@ -1,25 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-
 import studentsRoutes from './routes/students.js'
 import adminRoutes from './routes/admin.js'
-
 dotenv.config()
-
 const app = express()
-const PORT = process.env.PORT || 5000
-
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/students', studentsRoutes)
 app.use('/api/admin', adminRoutes)
-
 app.get('/', (req, res) => {
     res.json({ message: 'Server is working' })
 })
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+export default app
